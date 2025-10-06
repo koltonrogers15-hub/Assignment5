58734342f13ba081f86bf8579353e5583074e1a0
Kolton Rogers <kolton.rogers.15@gmail.com>
2025-10-05 19:59:02 -0600

    changed pop up symbology and mystuff values

--- week6Assignment/week6Assignment/map.js
+++ week6Assignment/week6Assignment/map.js
@@ -1,105 +1,81 @@
 var Main;
 
-// Assuming you're using a bundler or ES module-compatible environment
-
+// ESM imports
 import Map from "https://js.arcgis.com/4.33/@arcgis/core/Map.js";
 import Graphic from "https://js.arcgis.com/4.33/@arcgis/core/Graphic.js";
 import GraphicsLayer from "https://js.arcgis.com/4.33/@arcgis/core/layers/GraphicsLayer.js";
 import ElevationLayer from "https://js.arcgis.com/4.33/@arcgis/core/layers/ElevationLayer.js";
 import SceneView from "https://js.arcgis.com/4.33/@arcgis/core/views/SceneView.js";
 
+
+// Wrap everything in IIFE
 Main = (function() {
-    const layer = new ElevationLayer({
-        url: "http://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"
-    });
-    const map = new Map({
-        basemap: "hybrid",
-        ground: {
-            layers: [layer]
-         },
-    });
-    
-    const view = new SceneView({
-        container: "map",
-        viewingMode: "global",
-        map: map,
-        camera: {
-            position: {
-                x: -105.503,
-                y: 44.270,
-                z: 20000000,
-                spatialReference: {
-                    wkid: 4326
-    
-                }
-            },
-            heading: 0,
-            tilt: 0
-        },
-        popup: {
-            dockEnabled: true,
-            dockOptions: {
-                breakpoint: false
-            }
-        },
-        // enable shadows to be cast from the features
-        environment: {
-            lighting: {
-                directShadowsEnabled: false
-            }
-        }
-    });
-                
-    const initMap = function(){
-                          
-        const graphicsLayer = new GraphicsLayer();               
-        map.add(graphicsLayer);
 
-        for (const [key, value] of Object.entries(myStuff)){                       
-            console.log(key, value)
-                        
-            const point = {                        
-                type: "point",                             
-                x: value.coord[0],                        
-                y: value.coord[1],                            
-                z: 10000                          
-            };
-                                
-            const markerSymbol = {                            
-                type: "simple-marker",                             
-                color: [0, 0, 255],                            
-                outline: {
-                              
-                    // autocasts as new SimpleLineSymbol()                              
-                    color: [255, 255, 255],                             
-                    width: 2
-                            
-                }
-                          
-            };
-                                                
-            const pointGraphic = new Graphic({                            
-                geometry: point,                            
-                symbol: markerSymbol,                            
-                popupTemplate: {                                
-                    title: key + ": " + value.city + ", " + value.state                            
-                }                  
-            });
-                          
-            graphicsLayer.add(pointGraphic);
-                    
-                    
+  const layer = new ElevationLayer({
+    url: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer" // https
+  });
+
+  const map = new Map({
+    basemap: "hybrid",
+    ground: { layers: [layer] }
+  });
+
+  let view; // create inside init
+
+  const initMap = function(){
+    // Create the SceneView after the DOM is ready
+    view = new SceneView({
+      container: "map",
+      viewingMode: "global",
+      map: map,
+      camera: {
+        position: { x: -105.503, y: 44.270, z: 20000000, spatialReference: { wkid: 4326 } },
+        heading: 0,
+        tilt: 0
+      },
+      popup: {
+        dockEnabled: true,
+        dockOptions: { breakpoint: false }
+      },
+      environment: { lighting: { directShadowsEnabled: false } }
+    });
+
+    const graphicsLayer = new GraphicsLayer({
+      elevationInfo: { mode: "absolute-height" } // respects z
+    });
+    map.add(graphicsLayer);
+
+    // define the WebStyleSymbol via autocast JSON
+    const pinSymbol = {
+      type: "web-style",
+      name: "Pushpin 3",
+      styleName: "EsriIconsStyle"
+    };
+
+    for (const [key, value] of Object.entries(window.myStuff)){  // note window.
+      console.log(key, value);
+
+      const point = {
+        type: "point",
+        longitude: value.coord[0], // use lon/lat in 3D
+        latitude:  value.coord[1],
+        z: 10000
+      };
+
+      const pinGraphic = new Graphic({
+        geometry: point,
+        symbol: pinSymbol,
+        popupTemplate: {
+          title: key + ": " + value.city + ", " + value.state
         }
-                                    
+      });
+
+      graphicsLayer.add(pinGraphic);
     }
-                
-    initMap()
-                
-    return {};
+  };
 
-            
+  // Run after DOM is built so #map exists
+  window.addEventListener("DOMContentLoaded", initMap);
+
+  return {};
 })();
-
-
-
-    
--- week6Assignment/week6Assignment/obj.js
+++ week6Assignment/week6Assignment/obj.js
@@ -1,8 +1,13 @@
 var myStuff = {
-    "Graduate School": {city: "Fairbanks", state: "Alaska", "coord": [-147.776749, 64.835365] },
-    "Postdoc Place": {city: "Miami", state: "Florida", "coord": [-80.191788, 25.761681]},
-    "Current Residence": {city: "Laramie", state: "Wyoming", "coord": [-105.502, 41.3104]},
-    "Favorite Vacation": {city: "San Diego", state: "California", "coord": [-117.161087, 32.715736] }
+    "Graduate School": {city: "Laramie", state: "Wyoming", "coord": [-105.5911, 41.3114] },
+    "Birthplace": {city: "Evanston", state: "Wyoming", "coord": [-110.9632, 41.2680]},
+    "Favorite City": {city: "Las Vegas", state: "Nevada", "coord": [-115.1398, 36.1699]},
+    "Favorite Vacation": {city: "San Juan", state: "Puerto Rico", "coord": [-66.1057, 18.4663] }
+
+
+
+
+
 }
 
 
